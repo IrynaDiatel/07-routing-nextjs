9ebc33a6476476a6adf39b4da23d0b624bee5560
IrynaDiatel <kastyrinaira@gmail.com>
2026-05-31 12:11:48 +0300

    Add missing files

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import type { Note } from "@/types/note";
-import css from "./NotePreview.module.css";
-
+import css from "@/components/NotePreview/NotePreview.module.css";
 interface NotePreviewProps {
   note: Note;
 }
